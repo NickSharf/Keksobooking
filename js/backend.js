e0c2ae5b2c075dddf8d634f850c19d61b039e276
NickSharf <nikolai.sharf@mail.ru>
2020-04-06 10:37:57 +0300

    Обновляет ссылку на источник данных

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var SERVER_URL = 'https://js.dump.academy/keksobooking';
+  var SERVER_URL = 'https://javascript.pages.academy/keksobooking';
 
   var setup = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
